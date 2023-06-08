de46cdef413d9af6e59ae5aa115caa7cb1b9e4d3
Matheus Teixeira <contato.matheusteixeira@gmail.com>
2023-06-07 22:01:03 -0300

    Finalizado primeira parte do projeto

--- magic-seo.php
+++ magic-seo.php
@@ -1,14 +1,3 @@
 <?php
 
-use Aeusteixeira\MagicSeo\PreviewImage\PreviewImageGenerator;
-
 require __DIR__ . '/vendor/autoload.php';
-
-$generator = new PreviewImageGenerator('Título da Página', 'Descrição da Página', 'path/to/background-image.jpg');
-$result = $generator->generateImage('path/to/output-image.jpg');
-
-if ($result) {
-    echo 'Imagem gerada com sucesso!';
-} else {
-    echo 'Falha ao gerar a imagem.';
-}
